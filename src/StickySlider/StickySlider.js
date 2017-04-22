6089057dd89a6b96b660dbc9ca12db4354bc0f60
drabelpdx <drabelpdx@gmail.com>
2017-04-22 16:15:52 -0700

    Functional slider, props are passed

--- src/StickySlider/StickySlider.js
+++ src/StickySlider/StickySlider.js
@@ -2,32 +2,10 @@ import React from 'react';
 import Slider from 'rc-slider';
 // require('rc-slider/assets/index.css');
 
-const style = { width: 600, margin: 50 };
-const marks = {
-  1: {
-    label: <strong>2007</strong>,
-  },
-  2: '2008',
-  3: '2009',
-  4: '2010',
-  5: '2011',
-  6: '2012',
-  7: '2013',
-  8: '2014',
-  9: '2015',
-  10: {
-    label: <strong>2016</strong>,
-  },
-};
-
-function log(value) {
-  console.log(value); //eslint-disable-line
-}
-
-const StickySlider = ({ min, max, value, onChange }) => (
+const StickySlider = ({ min, max, value, marks, style, onChange }) => (
   <div style={style}>
     <p>My Sticky Slider</p>
-    <Slider min={1} max={10} marks={marks} step={1} onChange={log} defaultValue={8} />
+    <Slider min={min} max={max} marks={marks} tipFormatter={null} step={1} onChange={onChange} defaultValue={value} />
   </div>
 );
 
--- stories/StickySlider.story.js
+++ stories/StickySlider.story.js
@@ -8,20 +8,45 @@ const description = `
   This is some basic usage with the sticky slider component as built for Raise Effect, updated with changes for latest rc-slider. Due to the nature of the component, a higher order component is required to contain local state.
   Sliding should trigger changes that affect the local state.`;
 
+const style = { width: 600, margin: 50 };
+const marks = {
+  1: {
+    label: <strong>2007</strong>,
+  },
+  2: '2008',
+  3: '2009',
+  4: '2010',
+  5: '2011',
+  6: '2012',
+  7: '2013',
+  8: '2014',
+  9: '2015',
+  10: {
+    label: <strong>2016</strong>,
+  },
+};
+
+function log(value) {
+  console.log(value); //eslint-disable-line
+}
+
 const demoCode = () => (
   <StickySlider
-    min={0}
+    min={1}
     max={10}
-    value={3}
+    value={4}
+    marks={marks}
+    style={style}
+    onChange={log}
   />
 );
 
-
-// const propDocs = { inline: true, propTables: [StickySlider] };
+const propDocs = { inline: true, propTables: [StickySlider] };
 
 export default () => storiesOf(displayName, module)
   .addWithInfo(
     title,
     description,
     demoCode,
+    propDocs,
   );
